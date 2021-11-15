6783a5d9ec9b5a86c8c992e126864ce8c1623dd0
Phil G <fillupgalbraith@gmail.com>
2021-11-15 13:28:49 -0800

    ugly elements load from data with map iterator

--- potluck/src/components/Dashboard.js
+++ potluck/src/components/Dashboard.js
@@ -1,57 +1,91 @@
-import React from React.js
+import React, { useState, useEffect } from 'react';
+
+const myPotlucks = [ 
+  {
+    meetingName: 'Foodapaloosa',
+    people: [
+      {userName:'Abe123', dish: 'spaghetti'}, 
+      {userName:'Gabe234', dish: 'cookies'},
+      {userName:'Sal123', dish: 'biscuits'},
+      {userName:'Phil2', dish: 'pretzels'}
+    ],
+    organizer: 'Phil2',
+    date: '11/19/2021',
+    time: '12PM-1PM',
+    location: 'McArthur Park',
+    userDish: 'pretzels',
+    confirmed: false
+  },
+  {
+    meetingName: 'Feast Fest',
+    people: [
+      {userName:'Abe123', dish: 'apricots'}, 
+      {userName:'Gabe234', dish: 'bread bowls'},
+      {userName:'Sal123', dish: 'chowder'},
+      {userName:'Phil2', dish: 'turkey'}
+    ],
+    organizer: 'Sal123',
+    date: '11/26/2021',
+    time: '11AM-12PM',
+    location: 'BJHS staff lounge',
+    userDish: 'turkey',
+    confirmed: false
+  }
+]
+
 
 const Dashboard = () => {
-  const myPotlucks = [ 
-    {
-      meetingName: 'Foodapaloosa',
-      people: [
-        {userName:'Abe123', dish: 'spaghetti'}, 
-        {userName:'Gabe234', dish: 'cookies'},
-        {userName:'Sal123', dish: 'biscuits'},
-        {userName:'Phil2', dish: 'pretzels'}
-      ],
-      organizer: 'Phil2',
-      date: '11/19/2021',
-      time: '12PM-1PM',
-      location: 'McArthur Park',
-      userDish: 'pretzels',
-      confirmed: false
-    }
-  ]
+  const [confirmed, setConfirmed] = useState(false);
+  const [confirmText, setConfirmText] = useState('');
+
+  useEffect(() => {
+    confirmed? setConfirmText('Not Going? Cancel') : setConfirmText('Confirm You\'re Going!');
+  }, [confirmed]);
+
   const potluck1 = myPotlucks[0];
   const currentUser = 'Phil2';
+  const confirmClick = (e) => {
+      e.preventDefault();
+      setConfirmed(!confirmed);
+  }
   const newPotluck = () => {
       // link to new potluck component
   }
+  console.log(confirmed);
     return(
     <>
     <section id='dashboard'>
       <h1>Dashboard</h1>
-      <button onCick={newPotluck}>Create New Potluck</button>
+      <button onClick={newPotluck}>Create New Potluck</button>
     </section>
     
     <section id='myPotlucks' className='mtg-container'>
       <h2>My Potucks</h2>
-      <div className='meeting'>
-        <div className='info'>
-        <h3>Event: ${potluck1[meetingName]}</h3>
-        <ul>
-          <li>I'm bringing: ${potluck1[userDish]}</li>
-          <li>Date: ${potluck1[date]}</li>
-          <li>Time: ${potluck1[time]}</li>
-          <li>Location: ${potluck1[location]}</li>
-        </ul>
-        <div className='alert'>
-          <p>Confirm You're Going</p>
-          <input 
-            key={potluck1[meetingName]}
-            type='checkbox'
-            checked={potluck1.confirmed}
-          />
-        </div>
-      </div>
+      {
+        myPotlucks.map( potluck => {
+            return (
+          <div className='meeting'>
+            <div className='info'>
+              <h3>Event: {`${potluck['meetingName']}`}</h3>
+              <ul>
+                <li>I'm bringing: {`${potluck['userDish']}`}</li>
+                <li>Date:{`${potluck['date']}`}</li>
+                <li>Time:{`${potluck['time']}`}</li>
+                <li>Location: {`${potluck['location']}`}</li>
+              </ul>
+            </div>
+            <div className='alert'>
+              <button onClick={confirmClick}>
+                { `${confirmText}`}
+              </button>
+            </div>
+          </div>
+        )})
+    }
       
     </section>
     </>
   )  
 }
+
+export default Dashboard
