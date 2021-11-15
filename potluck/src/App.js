19f4c652fa1f842d4b08e7514835473822f76a2f
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-11-15 17:30:30 -0500

    working on login

--- potluck/src/App.js
+++ potluck/src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Link, Route } from 'react-router-dom';
+
+import Login from './components/Login';
+
+const initialLoginValues = {
+  username: '',
+  password: ''
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <nav>
+          <h1>Team Build</h1>
+        </nav>
       </header>
     </div>
   );
--- potluck/src/components/Login.js
+++ potluck/src/components/Login.js
@@ -1,11 +1,40 @@
 import React from "react";
 
-const Login = () => {
+const Login = (props) => {
+
+  const { username, password } = props.values;
+
+  const onChange = (e) => {
+    console.log(e);
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
     <div>
-      <form>
-        <input type="text" />
-        <input type="text" />
+      <h1>Login</h1>
+      <form id='login' onSubmit={onSubmit}>
+        <label> Username:
+          <input 
+            id='username-input'
+            type='text'
+            name='username' 
+            value={username}
+            onChange={onChange}
+          />
+        </label>
+
+        <label>
+          <input
+            id='password-input'
+            type='text'
+            name='password'
+            value={password}
+            onChange={onChange}
+          />
+        </label>
       </form>
     </div>
   );
