9a67ede25c06b85c90552e114056f879ee92569c
minasoha <minasoha@yahoo.com>
2021-11-17 13:08:10 -0500

    changed the div to a section on coverpage

--- potluck/src/components/CoverPage.js
+++ potluck/src/components/CoverPage.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 const CoverPage = () => {
   return (
     <div>
-      <h1>Potluck Planner</h1>
+      <h1 className="pageTitle">Potluck Planner</h1>
       <h3>Plan for every potluck</h3>
       <Link to="/login" className="button">
         Lets Plan
