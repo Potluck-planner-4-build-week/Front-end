9ffceb5bfadded558c5c21f0753a71ac62e033ef
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2021-11-17 16:57:48 -0500

    finished login validations

--- potluck/src/components/Login.js
+++ potluck/src/components/Login.js
@@ -1,19 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import formSchema from "./Validation/LoginFormSchema";
+import * as yup from 'yup';
 
 const initialLoginValues = {
   username: '',
   password: ''
 }
+const initialFormErrors = {
+  username: '',
+  password: ''
+};
 
 const Login = () => {
   //const { username, password } = props.values;
   const [loginValues, setLoginValues] = useState(initialLoginValues)
+  const [formErrors, setFormErrors] = useState(initialFormErrors);
+  const [disabled, setDisabled] = useState(true);
 
-  const onChange = (e) => {
-    setLoginValues({
-      ...loginValues,
-      [e.target.name]: e.target.value
-    })
+  const validate = (name, value) => {
+    yup.reach(formSchema, name)
+    .validate(value)
+    .then(() => setFormErrors({ ...formErrors, [name]: ''}))
+    .catch(err => setFormErrors({ ...formErrors, [name]: err.errors[0]}))
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setLoginValues({ ...loginValues, [name]: value })
+    validate(name, value);
     console.log(e.target.name)
   }
 
@@ -21,18 +35,26 @@ const Login = () => {
     e.preventDefault();
   }
 
+  useEffect(() => {
+    formSchema.isValid(loginValues).then(valid => setDisabled(!valid))
+  }, [loginValues])
   return (
   <section>
+
     <div className='login-page'>
       <h1 className='pageTitle'>Login</h1>
       <form id='login' onSubmit={onSubmit}>
+        <div>
+          <p>{formErrors.username}</p>
+          <p>{formErrors.password}</p>
+        </div>
         <label> Username:
           <input 
             id='username-input'
             type='text'
             name='username' 
             value={loginValues.username}
-            onChange={onChange}
+            onChange={handleChange}
           />
         </label>
 
@@ -42,11 +64,11 @@ const Login = () => {
             type='password'
             name='password'
             value={loginValues.password}
-            onChange={onChange}
+            onChange={handleChange}
           />
         </label>
 
-        <button>Submit</button>
+        <button disabled={disabled}>Submit</button>
       </form>
     </div>
   </section>
